5e3957de95df82efbbbff1b635f514d1fb2bfea3
Roman <roltan007@gmail.com>
2018-09-09 00:59:53 +0600

    Add 3 new method: max length vector in array, find vector in array and sum all vectors in array

--- src/september2018/Vector3DArray.java
+++ src/september2018/Vector3DArray.java
@@ -29,6 +29,43 @@ public class Vector3DArray {
         return vector3DArray;
     }
 
+    public double maxLength() {
+        double max = 0;
+        double length;
+
+        for (Vector3D i : vector3DArray) {
+            length = i.length();
+
+            if (length >= max) {
+                max = length;
+            }
+        }
+
+        return max;
+    }
+
+    public int find(Vector3D vector3D) {
+        int arrayLength = vector3DArray.length;
+
+        for (int i = 0; i < arrayLength; i++) {
+            if (vector3DArray[i].equalVectors(vector3D)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public Vector3D arraySum() {
+        Vector3D temp = new Vector3D();
+
+        for (Vector3D i : vector3DArray) {
+            temp = Vector3DProcessor.vectorsSum(i, temp);
+        }
+
+        return temp;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
