73333c072ffe9ad23479b9efd1eae479e6cd7c87
Roman <romanbelinsky99@gmail.com>
2018-10-25 16:46:23 +0600

    Add method for string multiplication

--- src/october2018/StringProcessor.java
+++ src/october2018/StringProcessor.java
@@ -1,4 +1,29 @@
 package october2018;
 
 public class StringProcessor {
+    public static String stringMultiplier(String multiplicationString, int multiplicationNumber) throws Exception {
+        if (multiplicationNumber < 0) {
+            throw new Exception("Multiplication number < 0");
+        }
+
+        if (multiplicationString == null) {
+            throw new Exception("Multiplication string is null");
+        }
+
+        if (multiplicationNumber == 0) {
+            return "";
+        }
+
+        if (multiplicationNumber == 1) {
+            return multiplicationString;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < multiplicationNumber; i++) {
+            sb.append(multiplicationString);
+        }
+
+        return sb.toString();
+    }
 }
