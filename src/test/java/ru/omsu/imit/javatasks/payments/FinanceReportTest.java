c4086ebf183f4c28f7cfb20418005a5f4645bea6
Roman <romanbelinsky99@gmail.com>
2018-12-09 03:04:23 +0600

    Add class for test FinanceReport

--- src/test/java/ru/omsu/imit/javatasks/payments/FinanceReportTest.java
+++ src/test/java/ru/omsu/imit/javatasks/payments/FinanceReportTest.java
@@ -1,4 +1,12 @@
 package ru.omsu.imit.javatasks.payments;
 
+import org.junit.Before;
+
 public class FinanceReportTest {
+    FinanceReport financeReport;
+
+    //TODO: Придумать как правильно сделать before
+    @Before
+    public void setUp() {
+    }
 }
