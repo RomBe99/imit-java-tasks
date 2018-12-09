c218496350f1e891d3df78a90e93abfaedd306ef
Roman <romanbelinsky99@gmail.com>
2018-12-10 01:52:14 +0600

    Complete class Functional

---
+++ src/main/java/ru/omsu/imit/javatasks/functions/Functional.java
@@ -0,0 +1,54 @@
+package ru.omsu.imit.javatasks.functions;
+
+public class Functional<T extends IFunction> implements IFunctional<T> {
+    private double beginValue;
+    private double endValue;
+    private T function;
+
+    public Functional(double beginValue, double endValue, T function) {
+        setBeginValue(beginValue);
+        setEndValue(endValue);
+        setFunction(function);
+    }
+
+    public double calculateValueInStartpoint() {
+        return function.calculus(beginValue);
+    }
+
+    public double calculateValueInEndpoint() {
+        return function.calculus(endValue);
+    }
+
+    public double calculateValueInMidpoint() {
+        return (calculateValueInStartpoint() + calculateValueInEndpoint()) / 2;
+    }
+
+    public void setBeginValue(double beginValue) {
+        this.beginValue = beginValue;
+    }
+
+    public void setEndValue(double endValue) {
+        this.endValue = endValue;
+    }
+
+    public void setFunction(T function) {
+        this.function = function;
+    }
+
+    public double getBeginValue() {
+        return beginValue;
+    }
+
+    public double getEndValue() {
+        return endValue;
+    }
+
+    public T getFunction() {
+        return function;
+    }
+
+    @Override
+    public int calculusFunctional(T function) {
+        return 0;
+    }
+}
