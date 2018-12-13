e25eb1f4734be6c661522bd6f1dd0555e7ebd810
Roman <romanbelinsky99@gmail.com>
2018-12-14 02:40:20 +0600

    Add fields top and bottom and add constructor, getters, setters, equals and hashCode

--- src/main/java/ru/omsu/imit/javatasks/functions/Function.java
+++ src/main/java/ru/omsu/imit/javatasks/functions/Function.java
@@ -1,5 +1,43 @@
 package ru.omsu.imit.javatasks.functions;
 
+import java.util.Objects;
+
 public abstract class Function implements IFunction {
-    
+    private double top;
+    private double bottom;
+
+    public Function(double top, double bottom) {
+        setTop(top);
+        setBottom(bottom);
+    }
+
+    public void setTop(double top) {
+        this.top = top;
+    }
+
+    public void setBottom(double bottom) {
+        this.bottom = bottom;
+    }
+
+    public double getTop() {
+        return top;
+    }
+
+    public double getBottom() {
+        return bottom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Function function = (Function) o;
+        return Double.compare(function.top, top) == 0 &&
+                Double.compare(function.bottom, bottom) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(top, bottom);
+    }
 }
