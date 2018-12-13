2b3a5b213438acb7a4f55567cf67782cf028230e
Roman <romanbelinsky99@gmail.com>
2018-12-14 02:29:24 +0600

    Update interface IFunction

--- src/main/java/ru/omsu/imit/javatasks/functions/IFunction.java
+++ src/main/java/ru/omsu/imit/javatasks/functions/IFunction.java
@@ -1,5 +1,7 @@
 package ru.omsu.imit.javatasks.functions;
 
 public interface IFunction {
-    double calculus(double value);
+    double calculus(double value) throws FunctionException;
+    double getTop();
+    double getBottom();
 }
