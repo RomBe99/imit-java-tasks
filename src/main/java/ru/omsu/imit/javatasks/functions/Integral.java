1e93d6d98693c94ae4f418a1e97989ab102c0ad0
Roman <romanbelinsky99@gmail.com>
2018-12-14 13:03:56 +0600

    Method calculate in progress

--- src/main/java/ru/omsu/imit/javatasks/functions/Integral.java
+++ src/main/java/ru/omsu/imit/javatasks/functions/Integral.java
@@ -1,12 +1,33 @@
 package ru.omsu.imit.javatasks.functions;
 
 public class Integral<T extends Function> extends Functional {
-    public Integral(double top, double bottom, Function function) throws FunctionalException {
+    final double epsilon = 0.01;
+
+    public Integral(double top, double bottom, T function) throws FunctionalException {
         super(top, bottom, function);
     }
 
     @Override
-    public double calculate(IFunction function) throws FunctionalException {
-        return 0;
+    public double calculate() throws FunctionException {
+        final double top = getTop();
+        final double bottom = getBottom();
+        final Function function = getFunction();
+
+        double segmentLength;
+        double currentValue = 0;
+        double previousValue = 0;
+
+        for (int i = 5; Math.abs(currentValue - previousValue) >= epsilon; i += 5) {
+            segmentLength = function.calculus(top) - function.calculus(bottom) / i;
+            previousValue = currentValue;
+            //todo integral sum
+//            currentValue = segmentLength * function.calculus();
+
+//            for (int j = 0; j < 5; j++) {
+//
+//            }
+        }
+
+        return currentValue;
     }
 }
