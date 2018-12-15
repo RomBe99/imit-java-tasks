df00289e089583e16fae7a4a766501fec4b45e4b
Roman <romanbelinsky99@gmail.com>
2018-12-15 23:55:59 +0600

    Now this exception extends from FunctionException

--- src/main/java/ru/omsu/imit/javatasks/functions/FunctionalException.java
+++ src/main/java/ru/omsu/imit/javatasks/functions/FunctionalException.java
@@ -1,4 +1,7 @@
 package ru.omsu.imit.javatasks.functions;
 
-public class FunctionalException extends Exception {
+public class FunctionalException extends FunctionException {
+    public FunctionalException(double value) {
+        super(value);
+    }
 }
