f9a9f55817a9fa85347e639a28364eae2a2beabe
Roman <romanbelinsky99@gmail.com>
2018-12-24 02:08:47 +0600

    Add constructor with string

--- src/main/java/ru/omsu/imit/javatasks/functions/FunctionException.java
+++ src/main/java/ru/omsu/imit/javatasks/functions/FunctionException.java
@@ -1,6 +1,10 @@
 package ru.omsu.imit.javatasks.functions;
 
 public class FunctionException extends Exception {
+    public FunctionException(String exceptionInfo) {
+        super(exceptionInfo);
+    }
+
     public FunctionException(double value) {
         super("Not in domain: " + value);
     }
--- src/main/java/ru/omsu/imit/javatasks/functions/FunctionalException.java
+++ src/main/java/ru/omsu/imit/javatasks/functions/FunctionalException.java
@@ -1,6 +1,10 @@
 package ru.omsu.imit.javatasks.functions;
 
 public class FunctionalException extends FunctionException {
+    public FunctionalException(String exceptionInfo) {
+        super(exceptionInfo);
+    }
+
     public FunctionalException(double value) {
         super(value);
     }
