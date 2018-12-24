f57b86d075e1d20c4f81b1c3f8a2c531254e2906
Roman <romanbelinsky99@gmail.com>
2018-12-24 23:33:11 +0600

    Add test for class LinearPolynomial

--- src/test/java/ru/omsu/imit/javatasks/functions/LinearPolynomialTest.java
+++ src/test/java/ru/omsu/imit/javatasks/functions/LinearPolynomialTest.java
@@ -1,4 +1,38 @@
 package ru.omsu.imit.javatasks.functions;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class LinearPolynomialTest {
+    private LinearPolynomial linearPolynomial;
+
+    @Before
+    public void setUp() throws Exception {
+        linearPolynomial = new LinearPolynomial(1, 2);
+    }
+
+
+    @Test
+    public void testLinearPolynomialCalculusCorrect() {
+        try {
+            double result = linearPolynomial.calculus(2);
+            assertEquals(5, (int) result);
+        } catch (Exception ex) {
+            fail();
+        }
+    }
+
+//    @Test
+//    public void testLinearPolynomialCalculusIncorrect() {
+//        try {
+//            exhibitor.calculus(20);
+//        } catch (FunctionException ex) {
+//            throw ex;
+//        } catch (Exception ex) {
+//            fail();
+//        }
+//    }
 }
