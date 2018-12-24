1cee7444397e3c472e057beb01980e8fd7deee81
Roman <romanbelinsky99@gmail.com>
2018-12-24 23:36:14 +0600

    Add constructors from Exception

--- src/main/java/ru/omsu/imit/javatasks/functions/FunctionException.java
+++ src/main/java/ru/omsu/imit/javatasks/functions/FunctionException.java
@@ -5,6 +5,17 @@ public class FunctionException extends Exception {
         super(exceptionInfo);
     }
 
+    public FunctionException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public FunctionException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public FunctionException() {
+    }
+
     public FunctionException(double value) {
         super("Not in domain: " + value);
     }
--- src/main/java/ru/omsu/imit/javatasks/functions/FunctionalException.java
+++ src/main/java/ru/omsu/imit/javatasks/functions/FunctionalException.java
@@ -5,6 +5,17 @@ public class FunctionalException extends FunctionException {
         super(exceptionInfo);
     }
 
+    public FunctionalException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public FunctionalException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public FunctionalException() {
+    }
+
     public FunctionalException(double value) {
         super(value);
     }
