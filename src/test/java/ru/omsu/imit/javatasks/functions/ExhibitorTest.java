d73d6e8a920def7070b6b3b4c6c7490028a6a4d1
Roman <romanbelinsky99@gmail.com>
2018-12-24 23:32:48 +0600

    Add test for class Exhibitor

--- src/test/java/ru/omsu/imit/javatasks/functions/ExhibitorTest.java
+++ src/test/java/ru/omsu/imit/javatasks/functions/ExhibitorTest.java
@@ -1,4 +1,39 @@
 package ru.omsu.imit.javatasks.functions;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class ExhibitorTest {
+    private Exhibitor exhibitor;
+    private final double DELTA = 0.01;
+
+    @Before
+    public void setUp() throws Exception {
+        exhibitor = new Exhibitor(new LinearPolynomial(1, 2), 4, 0);
+    }
+
+
+    @Test
+    public void testCalculusCorrect() {
+        try {
+            double result = exhibitor.calculus(2);
+            assertEquals(1 + 2 * Math.E * Math.E, result, DELTA);
+        } catch (Exception ex) {
+            fail();
+        }
+    }
+
+    @Test (expected = FunctionException.class)
+    public void testCalculusIncorrect() throws FunctionException {
+        try {
+            exhibitor.calculus(20);
+        } catch (FunctionException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            fail();
+        }
+    }
 }
