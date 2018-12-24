8890e0727b931f5a0a9c3bebbc93e8f8fcd32e5e
Roman <romanbelinsky99@gmail.com>
2018-12-25 03:28:42 +0600

    Repair integral, yay

--- src/main/java/ru/omsu/imit/javatasks/functions/Integral.java
+++ src/main/java/ru/omsu/imit/javatasks/functions/Integral.java
@@ -9,31 +9,29 @@ public class Integral<T extends Function> extends Functional {
     @Override
     public double calculate(Function function) throws FunctionException {
         try {
-            final double top = getTop();
+            final int segmentLength = 30;
             final double bottom = getBottom();
+            final double lengthSegment = (getTop() - bottom) / segmentLength;
+            double integral = 0;
 
-            int segmentLength = 5;
-            double newSquare = function.calculus(bottom);
-            double prevSquare;
-            double epsilon = 0.01;
+            for (int i = 0; i < segmentLength ; i++) {
+                integral += function.calculus(bottom + lengthSegment * (i + 0.5));
+            }
 
-            do {
-                prevSquare = newSquare;
-                newSquare = function.calculus(bottom + segmentLength * prevSquare);
-                segmentLength += 5;
-            } while (newSquare - prevSquare >= epsilon);
+            integral *= lengthSegment;
 
-            return (top - bottom) / segmentLength * newSquare;
+            return integral;
         } catch (FunctionException ex) {
             throw new FunctionalException("Can't calculus the function value in integral range", ex);
         }
     }
 
-//    public static void main(String[] args) throws FunctionException {
-//        LinearPolynomial l = new LinearPolynomial(1, 0);
-//        l.setDomain(3 , 1);
-//
-//        Integral integral = new Integral(3, 1);
-//        System.out.println(integral.calculate(l));
-//    }
+    public static void main(String[] args) {
+        try {
+            Integral integral = new Integral(2,0);
+            System.out.println(integral.calculate(new LinearPolynomial(1, 0)));
+        } catch (FunctionException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
