fa88dea9d76a21487e4dca7255cdadb65ad96f34
Roman <romanbelinsky99@gmail.com>
2018-12-24 23:36:47 +0600

    Work in progress

--- src/main/java/ru/omsu/imit/javatasks/functions/Integral.java
+++ src/main/java/ru/omsu/imit/javatasks/functions/Integral.java
@@ -1,6 +1,6 @@
 package ru.omsu.imit.javatasks.functions;
 
-public class Integral extends Functional {
+public class Integral<T extends Function> extends Functional {
 
     public Integral(double top, double bottom) throws FunctionalException {
         super(top, bottom);
@@ -8,36 +8,32 @@ public class Integral extends Functional {
 
     @Override
     public double calculate(Function function) throws FunctionException {
-        final double top = getTop();
-        if (!function.inDomain(top)) {
-            throw new FunctionalException(top);
+        try {
+            final double top = getTop();
+            final double bottom = getBottom();
+
+            int segmentLength = 5;
+            double newSquare = function.calculus(bottom);
+            double prevSquare;
+            double epsilon = 0.01;
+
+            do {
+                prevSquare = newSquare;
+                newSquare = function.calculus(bottom + segmentLength * prevSquare);
+                segmentLength += 5;
+            } while (newSquare - prevSquare >= epsilon);
+
+            return (top - bottom) / segmentLength * newSquare;
+        } catch (FunctionException ex) {
+            throw new FunctionalException("Can't calculus the function value in integral range", ex);
         }
-
-        final double bottom = getBottom();
-        if (!function.inDomain(bottom)) {
-            throw new FunctionalException(bottom);
-        }
-
-
-        int segmentLength = 5;
-        double newSquare = function.calculus(bottom);
-        double prevSquare;
-        double epsilon = 0.01;
-
-        do {
-            prevSquare = newSquare;
-            newSquare = function.calculus(bottom + segmentLength * prevSquare);
-            segmentLength += 5;
-        } while (newSquare - prevSquare >= epsilon);
-
-        return (top - bottom) / segmentLength * newSquare;
     }
 
-    public static void main(String[] args) throws FunctionException {
-        LinearPolynomial l = new LinearPolynomial(1, 0);
-        l.setDomain(3 , 1);
-
-        Integral integral = new Integral(3, 1);
-        System.out.println(integral.calculate(l));
-    }
+//    public static void main(String[] args) throws FunctionException {
+//        LinearPolynomial l = new LinearPolynomial(1, 0);
+//        l.setDomain(3 , 1);
+//
+//        Integral integral = new Integral(3, 1);
+//        System.out.println(integral.calculate(l));
+//    }
 }
