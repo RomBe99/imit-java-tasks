6355d3b29d70c63079eef5b8bb1839dc722b442a
Roman <romanbelinsky99@gmail.com>
2018-12-24 23:33:40 +0600

    Add class for test Integral

---
+++ src/test/java/ru/omsu/imit/javatasks/functions/IntegralTest.java
@@ -0,0 +1,11 @@
+package ru.omsu.imit.javatasks.functions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IntegralTest {
+    @Test
+    public void calculateIntegral() {
+    }
+}
