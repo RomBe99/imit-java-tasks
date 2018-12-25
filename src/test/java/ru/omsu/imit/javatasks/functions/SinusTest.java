0f118e594e61378fb7bb4e1f4f567c0f506665b0
Roman <romanbelinsky99@gmail.com>
2018-12-26 01:39:23 +0600

    Add tests for class Sinus

--- src/test/java/ru/omsu/imit/javatasks/functions/SinusTest.java
+++ src/test/java/ru/omsu/imit/javatasks/functions/SinusTest.java
@@ -1,4 +1,38 @@
 package ru.omsu.imit.javatasks.functions;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class SinusTest {
+    private Sinus sinus;
+    private final double DELTA = 0.01;
+
+    @Before
+    public void setUp() throws FunctionException {
+        sinus = new Sinus(new LinearPolynomial(2, 2), 2, -2);
+    }
+
+    @Test
+    public void testCalculusCorrect() {
+        try {
+            double result = sinus.calculus(2);
+            assertEquals(sinus.getCoefficients().getSecondCoefficient() * Math.sin(sinus.getCoefficients().getFirstCoefficient() * 2), result, DELTA);
+        } catch (Exception ex) {
+            fail();
+        }
+    }
+
+    @Test (expected = FunctionException.class)
+    public void testCalculusIncorrect() throws FunctionException {
+        try {
+            sinus.calculus(20);
+        } catch (FunctionException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            fail();
+        }
+    }
 }
