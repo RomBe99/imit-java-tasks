e88842900c7ad7ffb1f698aecea49bf5041832c1
Roman <romanbelinsky99@gmail.com>
2018-12-26 01:57:38 +0600

    Add preset for test Integral class

--- src/test/java/ru/omsu/imit/javatasks/functions/IntegralTest.java
+++ src/test/java/ru/omsu/imit/javatasks/functions/IntegralTest.java
@@ -1,11 +1,16 @@
 package ru.omsu.imit.javatasks.functions;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class IntegralTest {
-    @Test
-    public void calculateIntegral() {
+    private Integral integral;
+    private final double DELTA = 0.01;
+
+    @Before
+    public void setUp() throws FunctionalException {
+        integral = new Integral(4, 0);
     }
 }
