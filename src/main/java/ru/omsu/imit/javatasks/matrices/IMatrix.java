61b97628ad66100a2ccafc4a298f791dbc22a972
Roman <romanbelinsky99@gmail.com>
2019-02-20 21:07:43 +0600

    Add methods for get rows and columns

--- src/main/java/ru/omsu/imit/javatasks/matrices/IMatrix.java
+++ src/main/java/ru/omsu/imit/javatasks/matrices/IMatrix.java
@@ -4,4 +4,7 @@ public interface IMatrix {
     double calculateDeterminant();
     void setMatrixElem(final int i, final int j, final double value);
     double getMatrixElem(final int i, final int j);
+    double getMatrixElem(final int i);
+    int getRows();
+    int getColumns();
 }
