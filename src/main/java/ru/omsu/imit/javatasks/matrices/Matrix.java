f9e150c23c1f3b6b19d68620e16042532e025fe4
Roman <romanbelinsky99@gmail.com>
2019-03-01 00:35:48 +0600

    Update method for calculating det

--- src/main/java/ru/omsu/imit/javatasks/matrices/Matrix.java
+++ src/main/java/ru/omsu/imit/javatasks/matrices/Matrix.java
@@ -50,54 +50,59 @@ public class Matrix implements IMatrix {
             return determinant;
         }
 
-        double[] detMatrix = elements;
-        final int SIZE = rows;
+        Matrix matrixCopy = new Matrix(this);
         final double DELTA = 0.00001;
+        final int SIZE = matrixCopy.getRows();
+        boolean sign = false;
         double temp;
-        int index = 0;
-        int address;
-        int address2;
+        double temp2;
         determinant = 1;
 
-        for (int i = 0; i < SIZE - 1; i++) {
-            temp = detMatrix[i * columns - i];
+        for (int i = 0; i < SIZE; i++) {
+            temp = matrixCopy.getMatrixElem(i, i);
 
-            for (int j = 0; j < SIZE; j++) {
-                if (temp <= detMatrix[i * SIZE - i + j]) {
-                    temp = detMatrix[i * SIZE - i + j];
-                    index = j;
+            // TODO Проверка деления на 0 и перемещение столбцов
+
+            determinant *= (sign ? -temp : temp);
+
+            for (int j = i + 1; j < SIZE; j++) {
+                temp2 = matrixCopy.getMatrixElem(j, i);
+
+                for (int k = i; k < SIZE; k++) {
+                    matrixCopy.setMatrixElem(j, k, matrixCopy.getMatrixElem(j, k) - matrixCopy.getMatrixElem(i, k) / temp * temp2);
                 }
-            }
-
-            if (temp <= DELTA && temp >= 0) {
-                return 0;
-            }
-
-            determinant *= temp;
-            address2 = (i + 1) * SIZE - (i + 1) + index - 1;
-
-            for (int j = 0; j < SIZE; j++) {
-                address = (i + 1) * SIZE - (i + 1) + j + 1; // Следующая строка
-                detMatrix[address] = detMatrix[i * SIZE - i + j] / detMatrix[i * SIZE - i + index] * detMatrix[address2] - detMatrix[address];
             }
         }
 
         determinantIsCorrect = true;
+        System.out.println(DemoMatrix.matrixToTable(matrixCopy));
         return determinant;
     }
 
     @Override
     public void setMatrixElem(final int i, final int j, final double value) {
-        elements[i * columns - i + j] = value;
+        if (elements.length <= i || elements.length <= j) {
+            // throw  TODO Исключение
+        }
+
+        elements[i * columns + j] = value;
         determinantIsCorrect = false;
     }
 
     @Override
     public double getMatrixElem(final int i, final int j) {
-        return elements[i * columns - i + j];
+        if (elements.length <= i || elements.length <= j) {
+            // throw  TODO Исключение
+        }
+
+        return elements[i * columns + j];
     }
 
     public void setMatrixElem(final int i, final double value) {
+        if (elements.length <= i) {
+            // throw  TODO Исключение
+        }
+
         elements[i] = value;
         determinantIsCorrect = false;
     }
@@ -111,6 +116,10 @@ public class Matrix implements IMatrix {
     }
 
     private void setElements(double[] elements) {
+        if (elements == null) {
+            // throw  TODO Исключение
+        }
+
         this.elements = elements;
     }
 
@@ -123,6 +132,10 @@ public class Matrix implements IMatrix {
     }
 
     public double getMatrixElem(final int i) {
+        if (elements.length <= i) {
+            // throw  TODO Исключение
+        }
+
         return elements[i];
     }
 
@@ -141,9 +154,10 @@ public class Matrix implements IMatrix {
     public static void main(String[] args) {
         double[] arr =  {7, 3, 5, 6,
                 8, 2, 9, 4,
-                1, 0, 2, 4,
-                9, 5, 3, 1};
+                1, 5, 2, 4,
+                9, 0, 3, 1};
         Matrix matrix = new Matrix(4, arr);
+        System.out.println(DemoMatrix.matrixToTable(matrix));
         System.out.println(matrix.calculateDeterminant());
     }
 }
