99018ed8ed6f5b3b9495ca45b262f66b43be3290
Roman <romanbelinsky99@gmail.com>
2019-03-09 01:26:36 +0600

    Add test for DemoMatrix

--- src/test/java/ru/omsu/imit/javatasks/matrices/DemoMatrixTest.java
+++ src/test/java/ru/omsu/imit/javatasks/matrices/DemoMatrixTest.java
@@ -1,16 +1,45 @@
 package ru.omsu.imit.javatasks.matrices;
 
+import org.junit.Assert;
 import org.junit.Before;
-
-import java.io.File;
+import org.junit.Test;
 
 public class DemoMatrixTest {
     private Matrix matrix;
-    private final String PATH = "./src/test/java/ru/omsu/imit/javatasks/matrices/examples/";
-    private final File CORRECT_MATIX = new File(PATH + "CorrectMatrix.txt");
-    private final File INCORRECT_MATIX = new File(PATH + "IncorrectMatrix.txt");
 
     @Before
-    public void setUp() {
+    public void setUp() throws MatrixException {
+        double[] elements = new double[] {7, 3, 5, 6,
+                8, 2, 9, 4,
+                1, 5, 2, 4,
+                9, 0, 3, 1};
+
+        matrix = new Matrix(4, elements);
+    }
+
+    @Test
+    public void matrixToTableTest() {
+        final boolean result;
+        try {
+            result = DemoMatrix.matrixToTable(matrix).equals("7.0 3.0 5.0 6.0\n" +
+                    "8.0 2.0 9.0 4.0\n" +
+                    "1.0 5.0 2.0 4.0\n" +
+                    "9.0 0.0 3.0 1.0\n");
+            Assert.assertTrue(result);
+        } catch (MatrixException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void sumAllMatrixElementsTest() {
+        try {
+            final double RESULT = DemoMatrix.sumAllMatrixElements(matrix);
+            final double ELEM_SUM = 69;
+            final double DELTA = 0.0001;
+            Assert.assertEquals(ELEM_SUM, RESULT, DELTA);
+        } catch (MatrixException e) {
+            e.printStackTrace();
+        }
     }
 }
