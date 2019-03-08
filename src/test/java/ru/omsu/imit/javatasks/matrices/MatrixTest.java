9fc63d949feb9534a5e14d033461fc78c4197c32
Roman <romanbelinsky99@gmail.com>
2019-03-09 03:54:27 +0600

    Add tests for Matrix

--- src/test/java/ru/omsu/imit/javatasks/matrices/MatrixTest.java
+++ src/test/java/ru/omsu/imit/javatasks/matrices/MatrixTest.java
@@ -1,25 +1,60 @@
 package ru.omsu.imit.javatasks.matrices;
 
-import org.junit.Before;
-
-import java.io.File;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MatrixTest {
     private Matrix matrix;
-    private final String PATH = "./src/test/java/ru/omsu/imit/javatasks/matrices/examples/";
-    private final File CORRECT_MATIX = new File(PATH + "CorrectMatrix.txt");
-    private final File INCORRECT_MATIX = new File(PATH + "IncorrectMatrix.txt");
+    private final double DELTA = 0.0001;
 
-    @Before
-    public void setUp() {
+    @Test
+    public void calculateDeterminantTestWithZeros1() {
+        final double DET = 0;
+        final int SIZE = 4;
+        final double[] elements = new double[]{7, 0, 5, 6,
+                8, 0, 9, 4,
+                1, 0, 2, 4,
+                9, 0, 3, 1};
+
+        try {
+            matrix = new Matrix(SIZE, elements);
+            Assert.assertEquals(DET, matrix.calculateDeterminant(), DELTA);
+        } catch (MatrixException e) {
+            e.printStackTrace();
+        }
     }
 
-//    public static void main(String[] args) {
-//        double[] elems = new double[]{7, 3, 5, 6,
-//                8, 2, 9, 4,
-//                1, 5, 2, 4,
-//                9, 0, 3, 1};
-//        Matrix matrix = new Matrix(16, elems);
-//        System.out.println(matrix.calculateDeterminant());
-//    }
+    @Test
+    public void calculateDeterminantTestWithZeros2() {
+        final double DET = 0;
+        final int SIZE = 4;
+        final double[] elements = new double[]{0, 0, 0, 0,
+                8, 3, 9, 4,
+                1, 2, 2, 4,
+                9, 5, 3, 1};
+
+        try {
+            matrix = new Matrix(SIZE, elements);
+            Assert.assertEquals(DET, matrix.calculateDeterminant(), DELTA);
+        } catch (MatrixException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void calculateDeterminantTest() {
+        final double DET = 620;
+        final int SIZE = 4;
+        final double[] elements = new double[]{7, 3, 5, 6,
+                8, 2, 9, 4,
+                1, 5, 2, 4,
+                9, 0, 3, 1};
+
+        try {
+            matrix = new Matrix(SIZE, elements);
+            Assert.assertEquals(DET, matrix.calculateDeterminant(), DELTA);
+        } catch (MatrixException e) {
+            e.printStackTrace();
+        }
+    }
 }
