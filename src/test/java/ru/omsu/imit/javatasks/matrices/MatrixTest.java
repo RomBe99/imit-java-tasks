e9286b91cb26b2ee47d7a473ab050fd656cc64a1
Roman <romanbelinsky99@gmail.com>
2019-03-17 21:42:57 +0600

    Fix tests for matrix

--- src/test/java/ru/omsu/imit/javatasks/matrices/MatrixTest.java
+++ src/test/java/ru/omsu/imit/javatasks/matrices/MatrixTest.java
@@ -3,12 +3,14 @@ package ru.omsu.imit.javatasks.matrices;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.testng.Assert.fail;
+
 public class MatrixTest {
     private Matrix matrix;
     private final double DELTA = 0.0001;
 
     @Test
-    public void calculateDeterminantTestWithZeros1() {
+    public void calculateDeterminantTestWithZeros1() throws MatrixException {
         final double DET = 0;
         final int SIZE = 4;
         final double[] ELEMENTS = new double[]{7, 0, 5, 6,
@@ -16,17 +18,12 @@ public class MatrixTest {
                 1, 0, 2, 4,
                 9, 0, 3, 1};
 
-        try {
-            matrix = new Matrix(SIZE, ELEMENTS);
-            Assert.assertEquals(DET, matrix.calculateDeterminant(), DELTA);
-            Assert.assertArrayEquals(ELEMENTS, matrix.getElements(), DELTA);
-        } catch (MatrixException e) {
-            e.printStackTrace();
-        }
+        matrix = new Matrix(SIZE, ELEMENTS);
+        Assert.assertEquals(DET, matrix.calculateDeterminant(), DELTA);
     }
 
     @Test
-    public void calculateDeterminantTestWithZeros2() {
+    public void calculateDeterminantTestWithZeros2() throws MatrixException {
         final double DET = 0;
         final int SIZE = 4;
         final double[] ELEMENTS = new double[]{0, 0, 0, 0,
@@ -34,17 +31,24 @@ public class MatrixTest {
                 1, 2, 2, 4,
                 9, 5, 3, 1};
 
-        try {
-            matrix = new Matrix(SIZE, ELEMENTS);
-            Assert.assertEquals(DET, matrix.calculateDeterminant(), DELTA);
-            Assert.assertArrayEquals(ELEMENTS, matrix.getElements(), DELTA);
-        } catch (MatrixException e) {
-            e.printStackTrace();
-        }
+        matrix = new Matrix(SIZE, ELEMENTS);
+        Assert.assertEquals(DET, matrix.calculateDeterminant(), DELTA);
     }
 
     @Test
-    public void calculateDeterminantTest() {
+    public void calculateDeterminantTestWithZeros3() throws MatrixException {
+        final double DET = 0;
+        final int SIZE = 3;
+        final double[] ELEMENTS = new double[]{1, 2, 3,
+                                               0, 0, 5,
+                                               0, 0, 7};
+
+        matrix = new Matrix(SIZE, ELEMENTS);
+        Assert.assertEquals(DET, matrix.calculateDeterminant(), DELTA);
+    }
+
+    @Test
+    public void calculateDeterminantTest() throws MatrixException {
         final double DET = 620;
         final int SIZE = 4;
         final double[] ELEMENTS = new double[]{7, 3, 5, 6,
@@ -52,17 +56,12 @@ public class MatrixTest {
                 1, 5, 2, 4,
                 9, 0, 3, 1};
 
-        try {
-            matrix = new Matrix(SIZE, ELEMENTS);
-            Assert.assertEquals(DET, matrix.calculateDeterminant(), DELTA);
-            Assert.assertArrayEquals(ELEMENTS, matrix.getElements(), DELTA);
-        } catch (MatrixException e) {
-            e.printStackTrace();
-        }
+        matrix = new Matrix(SIZE, ELEMENTS);
+        Assert.assertEquals(DET, matrix.calculateDeterminant(), DELTA);
     }
 
     @Test
-    public void matrixMinorTest() {
+    public void matrixMinorTest() throws MatrixException {
         final int ROW = 0;
         final int COLUMN = 1;
 
@@ -76,17 +75,7 @@ public class MatrixTest {
                 1, 5, 2, 4,
                 9, 0, 3, 1};
 
-        try {
-            matrix = new Matrix(MATRIX_SIZE, MATRIX_ELEMENTS);
-            Assert.assertArrayEquals(MINOR_ELEMENTS, matrix.getMinor(ROW, COLUMN).getElements(), DELTA);
-        } catch (MatrixException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void test() throws MatrixException {
-        Matrix m = new Matrix(3, new double[]{1,2,3, 0,0,5, 0, 0, 7});
-        Assert.assertEquals(0.0, m.calculateDeterminant(), DELTA);
+        matrix = new Matrix(MATRIX_SIZE, MATRIX_ELEMENTS);
+        Assert.assertArrayEquals(MINOR_ELEMENTS, matrix.getMinor(ROW, COLUMN).getElements(), DELTA);
     }
 }
