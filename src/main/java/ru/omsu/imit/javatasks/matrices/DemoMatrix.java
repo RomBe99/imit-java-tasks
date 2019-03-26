8acc47f75f6c34a6c84c5b927cff11254321c143
Roman <romanbelinsky99@gmail.com>
2019-03-26 13:37:21 +0600

    Add methods matrixSurrealization and matrixDesurrealization (with gson help)

--- src/main/java/ru/omsu/imit/javatasks/matrices/DemoMatrix.java
+++ src/main/java/ru/omsu/imit/javatasks/matrices/DemoMatrix.java
@@ -1,5 +1,7 @@
 package ru.omsu.imit.javatasks.matrices;
 
+import com.google.gson.Gson;
+
 public class DemoMatrix {
     public static double sumAllMatrixElements(final Matrix matrix) throws MatrixException {
         final int MATRIX_SIZE = matrix.getSize() * matrix.getSize();
@@ -10,5 +12,13 @@ public class DemoMatrix {
         }
 
         return sum;
+    }
+
+    public static String matrixSurrealization(final Matrix matrix) {
+        return new Gson().toJson(matrix);
+    }
+
+    public static Matrix matrixDesurrealization(final String gsonMatrix) {
+        return new Gson().fromJson(gsonMatrix, Matrix.class);
     }
 }
