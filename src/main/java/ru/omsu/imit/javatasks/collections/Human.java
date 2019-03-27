df6edbde17e0eaf46a632a983f598d0ef122e0a6
Roman <romanbelinsky99@gmail.com>
2019-03-27 13:22:30 +0600

    Add fields surname, name and patronymic, add constructor, setters and getters, add methods equals, hashcode and toString

--- src/main/java/ru/omsu/imit/javatasks/collections/Human.java
+++ src/main/java/ru/omsu/imit/javatasks/collections/Human.java
@@ -1,4 +1,63 @@
 package ru.omsu.imit.javatasks.collections;
 
+import java.util.Objects;
+
 public class Human {
+    private String surname;
+    private String name;
+    private String patronymic;
+
+    public Human(String surname, String name, String patronymic) {
+        setSurname(surname);
+        setName(name);
+        setPatronymic(patronymic);
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        return Objects.equals(surname, human.surname) &&
+                Objects.equals(name, human.name) &&
+                Objects.equals(patronymic, human.patronymic);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(surname, name, patronymic);
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                '}';
+    }
 }
