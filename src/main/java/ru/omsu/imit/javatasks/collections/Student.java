a7206ac492d00aa92166ab611b88ea1aefff8819
Roman <romanbelinsky99@gmail.com>
2019-03-27 14:50:31 +0600

    Implement class Student

--- src/main/java/ru/omsu/imit/javatasks/collections/Student.java
+++ src/main/java/ru/omsu/imit/javatasks/collections/Student.java
@@ -1,4 +1,41 @@
 package ru.omsu.imit.javatasks.collections;
 
+import java.util.Objects;
+
 public class Student extends Human {
+    private String facultyТame;
+
+    public Student(final String surname, final String name, final String patronymic, final String facultyТame) {
+        super(surname, name, patronymic);
+        setFacultyТame(facultyТame);
+    }
+
+    public void setFacultyТame(final String facultyТame) {
+        this.facultyТame = facultyТame;
+    }
+
+    public String getFacultyТame() {
+        return facultyТame;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Student student = (Student) o;
+        return Objects.equals(facultyТame, student.facultyТame);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), facultyТame);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "facultyТame='" + facultyТame + '\'' +
+                '}';
+    }
 }
