cbbb31be7e9511fc233e9ff38b01e19afe721b69
Roman <romanbelinsky99@gmail.com>
2019-04-07 15:11:36 +0600

    Add method numberLinesStartingPerCharacter for counting strings begin on startChar

--- src/main/java/ru/omsu/imit/javatasks/collections/CollectionsDemo.java
+++ src/main/java/ru/omsu/imit/javatasks/collections/CollectionsDemo.java
@@ -1,4 +1,17 @@
 package ru.omsu.imit.javatasks.collections;
 
+import java.util.List;
+
 public class CollectionsDemo {
+    public static int numberLinesStartingPerCharacter(List<String> stringList, char startChar) {
+        int count = 0;
+
+        for (String string : stringList) {
+            if (string.charAt(0) == startChar) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
