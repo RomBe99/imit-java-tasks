219d46cdc65fd385dde997da43ef5c219b0d23c7
Roman <romanbelinsky99@gmail.com>
2019-04-07 16:23:16 +0600

    Add field strings and implement setUp

--- src/test/java/ru/omsu/imit/javatasks/collections/CollectionsDemoTest.java
+++ src/test/java/ru/omsu/imit/javatasks/collections/CollectionsDemoTest.java
@@ -1,4 +1,17 @@
 package ru.omsu.imit.javatasks.collections;
 
+import org.junit.Before;
+
+import java.util.ArrayList;
+
 public class CollectionsDemoTest {
+    private ArrayList<String> stringArrayList;
+
+    @Before
+    public void setUp() {
+        final int ARRAY_LIST_SIZE = 10;
+
+        stringArrayList = new ArrayList<>(ARRAY_LIST_SIZE);
+
+    }
 }
