b61faf2e183dd935db838353e27777c07046b1bc
Roman <romanbelinsky99@gmail.com>
2019-04-18 23:23:50 +0600

    Fix tests with new realization

--- src/test/java/ru/omsu/imit/javatasks/collections/FindNamesakesFromListTest.java
+++ src/test/java/ru/omsu/imit/javatasks/collections/FindNamesakesFromListTest.java
@@ -20,9 +20,9 @@ public class FindNamesakesFromListTest {
 
     @Test
     public void findNamesakesFromListTest1() {
-        final String SURNAME = "Vsileev";
+        final Human NAMESAKE = new Human("Vsileev", TEST_NAME, TEST_PATRONYMIC, TEST_AGE);
 
-        final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(HUMANS, SURNAME);
+        final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(HUMANS, NAMESAKE);
         final ArrayList<Human> EXPECTED = new ArrayList<>();
 
         Assert.assertEquals(EXPECTED, ACTUAL);
@@ -30,11 +30,11 @@ public class FindNamesakesFromListTest {
 
     @Test
     public void findNamesakesFromListTest2() {
-        final String SURNAME = "Shumov";
+        final Human NAMESAKE = new Human("Shumov", TEST_NAME, TEST_PATRONYMIC, TEST_AGE);
 
         final Human[] HUMANS_EXPECTED = {new Human("Shumov", TEST_NAME, TEST_PATRONYMIC, TEST_AGE)};
 
-        final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(HUMANS, SURNAME);
+        final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(HUMANS, NAMESAKE);
         final ArrayList<Human> EXPECTED = new ArrayList<>(Arrays.asList(HUMANS_EXPECTED));
 
         Assert.assertEquals(EXPECTED, ACTUAL);
@@ -42,12 +42,12 @@ public class FindNamesakesFromListTest {
 
     @Test
     public void findNamesakesFromListTest3() {
-        final String SURNAME = "Dubov";
+        final Human NAMESAKE = new Human("Dubov", TEST_NAME, TEST_PATRONYMIC, TEST_AGE);
 
         final Human[] HUMANS_EXPECTED = {new Human("Dubov", TEST_NAME, TEST_PATRONYMIC, TEST_AGE),
                 new Human("Dubov", TEST_NAME, TEST_PATRONYMIC, TEST_AGE)};
 
-        final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(HUMANS, SURNAME);
+        final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(HUMANS, NAMESAKE);
         final ArrayList<Human> EXPECTED = new ArrayList<>(Arrays.asList(HUMANS_EXPECTED));
 
         Assert.assertEquals(EXPECTED, ACTUAL);
@@ -55,13 +55,13 @@ public class FindNamesakesFromListTest {
 
     @Test
     public void findNamesakesFromListTest4() {
-        final String SURNAME = "Andreev";
+        final Human NAMESAKE = new Human("Andreev", TEST_NAME, TEST_PATRONYMIC, TEST_AGE);
 
         final Human[] HUMANS_EXPECTED = {new Human("Andreev", TEST_NAME, TEST_PATRONYMIC, TEST_AGE),
                 new Human("Andreev", TEST_NAME, TEST_PATRONYMIC, TEST_AGE),
                 new Human("Andreev", TEST_NAME, TEST_PATRONYMIC, TEST_AGE)};
 
-        final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(HUMANS, SURNAME);
+        final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(HUMANS, NAMESAKE);
         final ArrayList<Human> EXPECTED = new ArrayList<>(Arrays.asList(HUMANS_EXPECTED));
 
         Assert.assertEquals(EXPECTED, ACTUAL);
@@ -69,13 +69,13 @@ public class FindNamesakesFromListTest {
 
     @Test
     public void findNamesakesFromListTest5() {
-        final String SURNAME = "Andreev";
+        final Human NAMESAKE = new Human("Andreev", TEST_NAME, TEST_PATRONYMIC, TEST_AGE);
 
         final Human[] HUMANS_EXPECTED = {new Human("Andreev", TEST_NAME, TEST_PATRONYMIC, TEST_AGE),
                 new Human("Andreev", TEST_NAME, TEST_PATRONYMIC, TEST_AGE),
                 new Human("Andreev", TEST_NAME, TEST_PATRONYMIC, TEST_AGE)};
 
-        final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(HUMANS, SURNAME);
+        final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(HUMANS, NAMESAKE);
         final ArrayList<Human> EXPECTED = new ArrayList<>(Arrays.asList(HUMANS_EXPECTED));
 
         Assert.assertEquals(EXPECTED, ACTUAL);
