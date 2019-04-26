7b39f556b2caa7e8553d4bd8a779809e92981c6b
Roman <romanbelinsky99@gmail.com>
2019-04-26 23:40:00 +0600

    Refactor MaxAgeTest.java

--- src/test/java/ru/omsu/imit/javatasks/collections/MaxAgeTest.java
+++ src/test/java/ru/omsu/imit/javatasks/collections/MaxAgeTest.java
@@ -17,6 +17,7 @@ public class MaxAgeTest {
         final String TEST_PATRONYMIC = "Ivanovich";
         final String TEST_SURNAME = "Ivanov";
         final int TEST_AGE = 20;
+
         humans = new ArrayList<>(Arrays.asList(
                 new Human(TEST_SURNAME, TEST_NAME, TEST_PATRONYMIC, TEST_AGE),
                 new Human(TEST_SURNAME, TEST_NAME, TEST_PATRONYMIC, TEST_AGE),
@@ -29,10 +30,10 @@ public class MaxAgeTest {
     @Test
     public void maxAgeTest0() {
         final int SIZE = 1;
+        final int NEW_AGE = 20;
+
         HashSet<Human> excepted = new HashSet<>(SIZE);
         excepted.add(humans.get(0));
-
-        final int NEW_AGE = 20;
 
         for (Human human : humans) {
             human.setAge(NEW_AGE);
