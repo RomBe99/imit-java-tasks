68389d7c7e5312acbd007b9b2c45695c43fd3c21
Roman <romanbelinsky99@gmail.com>
2019-04-27 00:47:16 +0600

    Fix test findNamesakesFromListTest0 in FindNamesakesFromListTest.java

--- src/test/java/ru/omsu/imit/javatasks/collections/FindNamesakesFromListTest.java
+++ src/test/java/ru/omsu/imit/javatasks/collections/FindNamesakesFromListTest.java
@@ -23,9 +23,8 @@ public class FindNamesakesFromListTest {
         final Human NAMESAKE = new Human("Vsileev", TEST_NAME, TEST_PATRONYMIC, TEST_AGE);
 
         final ArrayList<Human> ACTUAL = CollectionsDemo.findNamesakesFromList(new ArrayList<Human>(), NAMESAKE);
-        final ArrayList<Human> EXPECTED = new ArrayList<>();
 
-        Assert.assertEquals(EXPECTED, ACTUAL);
+        Assert.assertNull(ACTUAL);
     }
 
     @Test
