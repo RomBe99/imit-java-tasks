8368ecae2559968d8886a0ff33aa8547ba809d57
Roman <romanbelinsky99@gmail.com>
2019-05-01 17:45:48 +0600

    Refactor test in MaxAgeTest.java

--- src/test/java/ru/omsu/imit/javatasks/collections/MaxAgeTest.java
+++ src/test/java/ru/omsu/imit/javatasks/collections/MaxAgeTest.java
@@ -31,11 +31,11 @@ public class MaxAgeTest {
 
     @Test
     public void maxAgeTest0() {
-        final int EXCEPTED_SIZE = 1;
+        final int EXPECTED_SIZE = 1;
         final int NEW_AGE = 20;
 
-        final Set<Human> EXCEPTED = new HashSet<>(EXCEPTED_SIZE);
-        EXCEPTED.add(humans.get(0));
+        final Set<Human> EXPECTED = new HashSet<>(EXPECTED_SIZE);
+        EXPECTED.add(humans.get(0));
 
         for (Human human : humans) {
             human.setAge(NEW_AGE);
@@ -46,45 +46,45 @@ public class MaxAgeTest {
         assert ACTUAL != null;
         final int ACTUAL_SET_SIZE = ACTUAL.size();
 
-        Assert.assertEquals(EXCEPTED_SIZE, ACTUAL_SET_SIZE);
-        Assert.assertEquals(EXCEPTED, ACTUAL);
+        Assert.assertEquals(EXPECTED_SIZE, ACTUAL_SET_SIZE);
+        Assert.assertEquals(EXPECTED, ACTUAL);
     }
 
     @Test
     public void maxAgeTest1() {
-        final int EXCEPTED_SIZE = 1;
+        final int EXPECTED_SIZE = 1;
         final int NEW_AGE = 30;
 
-        final Set<Human> EXCEPTED = new HashSet<>(EXCEPTED_SIZE);
+        final Set<Human> EXPECTED = new HashSet<>(EXPECTED_SIZE);
 
         humans.get(3).setAge(NEW_AGE);
-        EXCEPTED.add(humans.get(3));
+        EXPECTED.add(humans.get(3));
 
         final Set<Human> ACTUAL = CollectionsDemo.maxAge(humans);
 
         assert ACTUAL != null;
         final int ACTUAL_SET_SIZE = ACTUAL.size();
 
-        Assert.assertEquals(EXCEPTED_SIZE, ACTUAL_SET_SIZE);
-        Assert.assertEquals(EXCEPTED, ACTUAL);
+        Assert.assertEquals(EXPECTED_SIZE, ACTUAL_SET_SIZE);
+        Assert.assertEquals(EXPECTED, ACTUAL);
     }
 
     @Test
     public void maxAgeTest2() {
-        final int EXCEPTED_SIZE = 3;
+        final int EXPECTED_SIZE = 3;
         final String[] NAMES = {"Roman", "Stanislav", "Igor"};
         final int NEW_AGE = 30;
         final int[] HUMAN_NUMBERS = {1, 3, 5};
 
-        final Set<Human> EXCEPTED = new HashSet<>(EXCEPTED_SIZE);
+        final Set<Human> EXPECTED = new HashSet<>(EXPECTED_SIZE);
 
-        for (int i = 0; i < EXCEPTED_SIZE; i++) {
+        for (int i = 0; i < EXPECTED_SIZE; i++) {
             humans.get(HUMAN_NUMBERS[i]).setName(NAMES[i]);
         }
 
         for (int i : HUMAN_NUMBERS) {
             humans.get(i).setAge(NEW_AGE);
-            EXCEPTED.add(humans.get(i));
+            EXPECTED.add(humans.get(i));
         }
 
         final Set<Human> ACTUAL = CollectionsDemo.maxAge(humans);
@@ -92,7 +92,7 @@ public class MaxAgeTest {
         assert ACTUAL != null;
         final int ACTUAL_SET_SIZE = ACTUAL.size();
 
-        Assert.assertEquals(EXCEPTED_SIZE, ACTUAL_SET_SIZE);
-        Assert.assertEquals(EXCEPTED, ACTUAL);
+        Assert.assertEquals(EXPECTED_SIZE, ACTUAL_SET_SIZE);
+        Assert.assertEquals(EXPECTED, ACTUAL);
     }
 }
