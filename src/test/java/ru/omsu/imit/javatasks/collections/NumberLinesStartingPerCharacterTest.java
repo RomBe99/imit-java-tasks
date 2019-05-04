93b5c7c41500112047a485438052f6170f803de8
Roman <romanbelinsky99@gmail.com>
2019-05-04 15:51:07 +0600

    Rewrite tests with TestNG

--- src/test/java/ru/omsu/imit/javatasks/collections/NumberLinesStartingPerCharacterTest.java
+++ src/test/java/ru/omsu/imit/javatasks/collections/NumberLinesStartingPerCharacterTest.java
@@ -1,68 +1,32 @@
 package ru.omsu.imit.javatasks.collections;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class NumberLinesStartingPerCharacterTest {
-    private final List<String> STRING_ARRAY_LIST = new ArrayList<>(Arrays.asList(
-            "John", "Ken", "Oscar", "Jasmine", "Olivia", "Ollie", "Kai", "Victoria", "Jessica", "Oliver"));
+public class NumberLinesStartingPerCharacterTest extends Assert {
 
-    @Test
-    public void numberLinesStartingPerCharacterTest0() {
-        final List<String> STRING_ARRAY_LIST = new ArrayList<>();
-        final int EXPECTED = 0;
-        final char START_CHAR = 'O';
-        final int ACTUAL = CollectionsDemo.numberLinesStartingPerCharacter(STRING_ARRAY_LIST, START_CHAR);
-
-        Assert.assertEquals(EXPECTED, ACTUAL);
+    @DataProvider
+    public Object[][] numberLinesStartingPerCharacterTestData() {
+        final List<String> STRING_ARRAY_LIST = new ArrayList<>(Arrays.asList(
+            "John", "", "Ken", "Oscar", null, "Jasmine", "Olivia", "Ollie", "Kai", "Victoria", "Jessica", "Oliver"));
+        return new Object[][]{
+                {new ArrayList<String>(), '7', 0},
+                {STRING_ARRAY_LIST, 'z', 0},
+                {STRING_ARRAY_LIST, 'V', 1},
+                {STRING_ARRAY_LIST, 'K', 2}
+        };
     }
 
-    @Test
-    public void numberLinesStartingPerCharacterTest1() {
-        final int EXPECTED = 0;
-        final char START_CHAR = 'z';
-        final int ACTUAL = CollectionsDemo.numberLinesStartingPerCharacter(STRING_ARRAY_LIST, START_CHAR);
+    @Test(dataProvider = "numberLinesStartingPerCharacterTestData")
+    public void numberLinesStartingPerCharacterTest(
+            final List<String> listForTest, final char startCharForTest, final int expected) {
+        final int ACTUAL = CollectionsDemo.numberLinesStartingPerCharacter(listForTest, startCharForTest);
 
-        Assert.assertEquals(EXPECTED, ACTUAL);
-    }
-
-    @Test
-    public void numberLinesStartingPerCharacterTest2() {
-        final int EXPECTED = 1;
-        final char START_CHAR = 'V';
-        final int ACTUAL = CollectionsDemo.numberLinesStartingPerCharacter(STRING_ARRAY_LIST, START_CHAR);
-
-        Assert.assertEquals(EXPECTED, ACTUAL);
-    }
-
-    @Test
-    public void numberLinesStartingPerCharacterTest3() {
-        final int EXPECTED = 2;
-        final char START_CHAR = 'K';
-        final int ACTUAL = CollectionsDemo.numberLinesStartingPerCharacter(STRING_ARRAY_LIST, START_CHAR);
-
-        Assert.assertEquals(EXPECTED, ACTUAL);
-    }
-
-    @Test
-    public void numberLinesStartingPerCharacterTest4() {
-        final int EXPECTED = 3;
-        final char START_CHAR = 'J';
-        final int ACTUAL = CollectionsDemo.numberLinesStartingPerCharacter(STRING_ARRAY_LIST, START_CHAR);
-
-        Assert.assertEquals(EXPECTED, ACTUAL);
-    }
-
-    @Test
-    public void numberLinesStartingPerCharacterTest5() {
-        final int EXPECTED = 4;
-        final char START_CHAR = 'O';
-        final int ACTUAL = CollectionsDemo.numberLinesStartingPerCharacter(STRING_ARRAY_LIST, START_CHAR);
-
-        Assert.assertEquals(EXPECTED, ACTUAL);
+        assertEquals(ACTUAL, expected);
     }
 }
