c909ae13997da8121ca3ebc4a57bcc2afc050cb1
Roman <romanbelinsky99@gmail.com>
2019-05-07 23:50:38 +0600

    Rewrite tests in IdToAgeTest.java with TestNG

--- src/test/java/ru/omsu/imit/javatasks/collections/IdToAgeTest.java
+++ src/test/java/ru/omsu/imit/javatasks/collections/IdToAgeTest.java
@@ -1,129 +1,48 @@
 package ru.omsu.imit.javatasks.collections;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
-public class IdToAgeTest {
-    private Map<Integer, Human> humanHashMapForTest;
-    private Set<Integer> filterForTest;
-    private int[] keysForFilter;
+public class IdToAgeTest extends Assert {
 
-    @Before
-    public void setUp() {
+    @DataProvider
+    public Object[][] idToAgeTestData() {
         final String TEST_SURNAME = "Ivanov";
         final String TEST_NAME = "Ivan";
         final String TEST_PATRONYMIC = "Ivanovich";
         final int[] AGES = {3, 39, 25, 32, 39, 21};
         final int[] KEYS = {41, 28, 35, 22, 9, 49};
-        final int HASH_MAP_SIZE = 6;
+        final int MAP_SIZE = 6;
 
-        humanHashMapForTest = new HashMap<>(HASH_MAP_SIZE);
-
-        for (int i = 0; i < HASH_MAP_SIZE; i++) {
-            humanHashMapForTest.put(KEYS[i], new Human(TEST_SURNAME, TEST_NAME, TEST_PATRONYMIC, AGES[i]));
-        }
-    }
-
-    @Test
-    public void idToAgeTest0() {
-        final Map<Integer, Integer> ACTUAL = CollectionsDemo.idToAge(new HashMap<Integer, Human>(), new HashSet<Integer>());
-        final Map<Integer, Integer> EXPECTED = new HashMap<>();
-
-        Assert.assertEquals(EXPECTED, ACTUAL);
-    }
-
-    @Test
-    public void idToAgeTest1() {
-        final Map<Integer, Integer> EXPECTED = new HashMap<>();
-        final Map<Integer, Integer> ACTUAL = CollectionsDemo.idToAge(humanHashMapForTest, new HashSet<Integer>());
-
-        Assert.assertEquals(EXPECTED, ACTUAL);
-    }
-
-    @Test
-    public void idToAgeTest2() {
-        final int EXPECTED_HASH_MAP_SIZE = 0;
-        final int FILTER_FOR_TEST_SIZE = 3;
-        final Map<Integer, Integer> EXPECTED = new HashMap<>();
-
-        keysForFilter = new int[]{2, 20, 40};
-
-        filterForTest = new HashSet<>(FILTER_FOR_TEST_SIZE);
-
-        for (int i = 0; i < FILTER_FOR_TEST_SIZE; i++) {
-            filterForTest.add(keysForFilter[i]);
+        final Map<Integer, Human> HUMAN_MAP_FOR_TEST = new HashMap<>(MAP_SIZE);
+        for (int i = 0; i < MAP_SIZE; i++) {
+            HUMAN_MAP_FOR_TEST.put(KEYS[i], new Human(TEST_SURNAME, TEST_NAME, TEST_PATRONYMIC, AGES[i]));
         }
 
-        final Map<Integer, Integer> ACTUAL = CollectionsDemo.idToAge(humanHashMapForTest, filterForTest);
+        final Map<Integer, Integer> EXPECTED_LIST = new HashMap<>(MAP_SIZE);
+        for (int i = 0; i < MAP_SIZE; i++) {
+            EXPECTED_LIST.put(KEYS[i], AGES[i]);
+        }
+
+        return new Object[][]{
+                {new HashMap<Integer, Human>(), new HashMap<Integer, Integer>()},
+                {HUMAN_MAP_FOR_TEST, EXPECTED_LIST}
+        };
+    }
+
+    @Test(dataProvider = "idToAgeTestData")
+    public void idToAgeTest(final Map<Integer, Human> humanMapForTest, final Map<Integer, Human> expectedList) {
+        final Map<Integer, Integer> ACTUAL = CollectionsDemo.idToAge(humanMapForTest);
+        final int EXPECTED_MAP_SIZE = expectedList.size();
 
         assert ACTUAL != null;
         final int ACTUAL_SIZE = ACTUAL.size();
 
-        Assert.assertEquals(EXPECTED_HASH_MAP_SIZE, ACTUAL_SIZE);
-        Assert.assertEquals(EXPECTED, ACTUAL);
-    }
-
-    @Test
-    public void idToAgeTest3() {
-        final int EXPECTED_HASH_MAP_SIZE = 4;
-        final int FILTER_FOR_TEST_SIZE = 4;
-        final int[] KEYS = {41, 28, 35, 22, 9, 49};
-
-        final Map<Integer, Integer> EXPECTED = new HashMap<>(EXPECTED_HASH_MAP_SIZE);
-
-        for (int i = 0; i < EXPECTED_HASH_MAP_SIZE; i++) {
-            EXPECTED.put(KEYS[i], humanHashMapForTest.get(KEYS[i]).getAge());
-        }
-
-        keysForFilter = new int[]{41, 35, 9, 49};
-
-        filterForTest = new HashSet<>(FILTER_FOR_TEST_SIZE);
-
-        for (int i = 0; i < FILTER_FOR_TEST_SIZE; i++) {
-            filterForTest.add(KEYS[i]);
-        }
-
-        final Map<Integer, Integer> ACTUAL = CollectionsDemo.idToAge(humanHashMapForTest, filterForTest);
-
-        assert ACTUAL != null;
-        final int ACTUAL_SIZE = ACTUAL.size();
-
-        Assert.assertEquals(EXPECTED_HASH_MAP_SIZE, ACTUAL_SIZE);
-        Assert.assertEquals(EXPECTED, ACTUAL);
-    }
-
-    @Test
-    public void idToAgeTest4() {
-        final int EXPECTED_HASH_MAP_SIZE = 6;
-        final int FILTER_FOR_TEST_SIZE = 6;
-        final int[] KEYS = {41, 28, 35, 22, 9, 49};
-
-        final Map<Integer, Integer> EXPECTED = new HashMap<>(EXPECTED_HASH_MAP_SIZE);
-
-        for (int i = 0; i < EXPECTED_HASH_MAP_SIZE; i++) {
-            EXPECTED.put(KEYS[i], humanHashMapForTest.get(KEYS[i]).getAge());
-        }
-
-        keysForFilter = new int[]{41, 28, 35, 22, 9, 49};
-
-        filterForTest = new HashSet<>(FILTER_FOR_TEST_SIZE);
-
-        for (int i = 0; i < FILTER_FOR_TEST_SIZE; i++) {
-            filterForTest.add(KEYS[i]);
-        }
-
-        final Map<Integer, Integer> ACTUAL = CollectionsDemo.idToAge(humanHashMapForTest, filterForTest);
-
-        assert ACTUAL != null;
-        final int ACTUAL_SIZE = ACTUAL.size();
-
-        Assert.assertEquals(EXPECTED_HASH_MAP_SIZE, ACTUAL_SIZE);
-        Assert.assertEquals(EXPECTED, ACTUAL);
+        assertEquals(ACTUAL_SIZE, EXPECTED_MAP_SIZE);
+        assertEquals(ACTUAL, expectedList);
     }
 }
