824e477cc56f75f5bb48c84d0b5a005a10a67695
Roman <romanbelinsky99@gmail.com>
2019-05-20 20:12:41 +0600

    Add tests for method numberOfPeopleOnList in ReflectionTest.java

---
+++ src/test/java/ru/omsu/imit/javatasks/ReflectionTest.java
@@ -0,0 +1,41 @@
+package ru.omsu.imit.javatasks;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ru.omsu.imit.javatasks.collections.Human;
+import ru.omsu.imit.javatasks.collections.Student;
+import ru.omsu.imit.javatasks.reflection.ReflectionDemo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReflectionTest extends Assert {
+
+    @DataProvider
+    public Object[][] numberOfPeopleOnListTestData() {
+        final Human HUMAN_FOR_TEST = new Human("Test", "Test", "Test", 20);
+        final Student STUDENT_FOR_TEST = new Student("Test", "Test", "Test", 20, "Test");
+
+        final Object[] OBJECTS = {"", null, STUDENT_FOR_TEST, HUMAN_FOR_TEST, STUDENT_FOR_TEST};
+        final Human[] HUMANS = {HUMAN_FOR_TEST, HUMAN_FOR_TEST, HUMAN_FOR_TEST, HUMAN_FOR_TEST, HUMAN_FOR_TEST, HUMAN_FOR_TEST};
+        final Student[] STUDENTS = {STUDENT_FOR_TEST, STUDENT_FOR_TEST, STUDENT_FOR_TEST, STUDENT_FOR_TEST, STUDENT_FOR_TEST, STUDENT_FOR_TEST, STUDENT_FOR_TEST, STUDENT_FOR_TEST};
+        final Human[] HUMANS_AND_STUDENTS = {STUDENT_FOR_TEST, HUMAN_FOR_TEST, STUDENT_FOR_TEST, STUDENT_FOR_TEST, HUMAN_FOR_TEST, HUMAN_FOR_TEST, STUDENT_FOR_TEST};
+
+
+        return new Object[][]{
+                {new ArrayList<>(Arrays.asList(OBJECTS)), 3},
+                {new ArrayList<>(Arrays.asList(HUMANS)), HUMANS.length},
+                {new ArrayList<>(Arrays.asList(STUDENTS)), STUDENTS.length},
+                {new ArrayList<>(Arrays.asList(HUMANS_AND_STUDENTS)), HUMANS_AND_STUDENTS.length}
+        };
+    }
+
+    @Test(dataProvider = "numberOfPeopleOnListTestData")
+    public void reflectionTest(final List<?> listForSearch, final int expected) {
+        final int ACTUAL = ReflectionDemo.numberOfPeopleOnList(listForSearch);
+
+        assertEquals(ACTUAL, expected);
+    }
+}
