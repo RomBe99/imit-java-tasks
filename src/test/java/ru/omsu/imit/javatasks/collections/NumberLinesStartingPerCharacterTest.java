13c6dae070d931b0cd23b178395c967f4d326c1d
Roman <romanbelinsky99@gmail.com>
2019-05-20 19:00:38 +0600

    Fix method deleteHumanFromList in CollectionsDemo.java

--- src/test/java/ru/omsu/imit/javatasks/collections/NumberLinesStartingPerCharacterTest.java
+++ src/test/java/ru/omsu/imit/javatasks/collections/NumberLinesStartingPerCharacterTest.java
@@ -14,6 +14,7 @@ public class NumberLinesStartingPerCharacterTest extends Assert {
     public Object[][] numberLinesStartingPerCharacterTestData() {
         final List<String> STRING_ARRAY_LIST = new ArrayList<>(Arrays.asList(
             "John", "", "Ken", "Oscar", null, "Jasmine", "Olivia", "Ollie", "Kai", "Victoria", "Jessica", "Oliver"));
+
         return new Object[][]{
                 {new ArrayList<String>(), '7', 0},
                 {STRING_ARRAY_LIST, 'z', 0},
@@ -23,8 +24,8 @@ public class NumberLinesStartingPerCharacterTest extends Assert {
     }
 
     @Test(dataProvider = "numberLinesStartingPerCharacterTestData")
-    public void numberLinesStartingPerCharacterTest(
-            final List<String> listForTest, final char startCharForTest, final int expected) {
+    public void numberLinesStartingPerCharacterTest
+            (final List<String> listForTest, final char startCharForTest, final int expected) {
         final int ACTUAL = CollectionsDemo.numberLinesStartingPerCharacter(listForTest, startCharForTest);
 
         assertEquals(ACTUAL, expected);
