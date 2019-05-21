3ed52f43d51101b11c66a48a8ee5a2006e51de1c
Roman <romanbelinsky99@gmail.com>
2019-05-21 22:23:50 +0600

    Rewrite methods in class ByteStreams.java

--- src/main/java/ru/omsu/imit/javatasks/io/ByteStreams.java
+++ src/main/java/ru/omsu/imit/javatasks/io/ByteStreams.java
@@ -1,37 +1,33 @@
 package ru.omsu.imit.javatasks.io;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class ByteStreams {
-    public static byte[] intArrayToByteArray(final int[] arrayForWrite) {
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-
-            for (int i : arrayForWrite) {
-                baos.write(i);
+    public static void arrayIntsToBinaryStream(final OutputStream os, final int[] ints) {
+        try (DataOutputStream dos = new DataOutputStream(os)) {
+            for(int i : ints) {
+                dos.writeInt(i);
             }
-
-            return baos.toByteArray();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return new byte[arrayForWrite.length];
     }
 
-    public static int[] byteArrayToIntArray(final byte[] arrayForRead) {
-        final int SIZE = arrayForRead.length;
-        int[] ints = new int[SIZE];
+    public static int[] arrayIntsFromBinaryStream(final InputStream is, final int n) {
+        int[] arr = new int[n];
 
-        try (ByteArrayInputStream bais = new ByteArrayInputStream(arrayForRead)) {
-            for (int i = 0; i < SIZE; i++) {
-                ints[i] = bais.read();
+        try(DataInputStream dis = new DataInputStream(is)) {
+            for (int i = 0; i < n; i++) {
+                arr[i] = dis.read();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return ints;
+        return arr;
     }
 }
