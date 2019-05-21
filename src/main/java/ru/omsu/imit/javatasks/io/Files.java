48fe6f4ddca6d572f028575d498e0f54f325c83d
Roman <romanbelinsky99@gmail.com>
2019-05-21 23:40:06 +0600

    Add method filesListWithSpecifiedExtension in class Files.java

--- src/main/java/ru/omsu/imit/javatasks/io/Files.java
+++ src/main/java/ru/omsu/imit/javatasks/io/Files.java
@@ -3,6 +3,9 @@ package ru.omsu.imit.javatasks.io;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class Files {
     public static int[] arrayIntsFromRandomAccessFile(final File fileForRead, final int initialBitShift) {
@@ -22,5 +25,9 @@ public class Files {
         }
 
         return new int[0];
+    }
+
+    public static List<String> filesListWithSpecifiedExtension(final File catalogPath, String extension) {
+        return Arrays.asList(Objects.requireNonNull(catalogPath.list((directory, name) -> name.endsWith(extension))));
     }
 }
