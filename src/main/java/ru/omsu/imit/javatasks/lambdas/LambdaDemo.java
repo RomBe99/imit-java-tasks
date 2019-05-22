4249feb76ea91698e31eff98e6869ba6dc7eecd1
Roman <romanbelinsky99@gmail.com>
2019-05-22 19:11:30 +0600

    Add lambda-expression makeHumanOlder in LambdaDemo

--- src/main/java/ru/omsu/imit/javatasks/lambdas/LambdaDemo.java
+++ src/main/java/ru/omsu/imit/javatasks/lambdas/LambdaDemo.java
@@ -1,7 +1,10 @@
 package ru.omsu.imit.javatasks.lambdas;
 
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public class LambdaDemo {
     public static Predicate<String> notContainsSpaces = string -> string != null && !string.contains(" ");
+    public static UnaryOperator<Human> makeHumanOlder = human ->
+            new Human(human.getSurname(), human.getName(), human.getPatronymic(), human.getAge() + 1, human.getSex());
 }
