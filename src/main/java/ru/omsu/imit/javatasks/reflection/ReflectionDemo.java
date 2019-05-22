255553e82af1f27f4be81a328194473db40c7368
Roman <romanbelinsky99@gmail.com>
2019-05-22 14:25:10 +0600

    Add method numberPublicClassMethds

--- src/main/java/ru/omsu/imit/javatasks/reflection/ReflectionDemo.java
+++ src/main/java/ru/omsu/imit/javatasks/reflection/ReflectionDemo.java
@@ -3,12 +3,20 @@ package ru.omsu.imit.javatasks.reflection;
 import ru.omsu.imit.javatasks.collections.Human;
 import ru.omsu.imit.javatasks.collections.Student;
 
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.List;
 
 public class ReflectionDemo {
     public static int numberOfPeopleOnList(List<?> listForSearch) {
         return (int) listForSearch.stream()
                 .filter(o -> o != null && (o.getClass() == Human.class || o.getClass() == Student.class))
+                .count();
+    }
+
+    public static int numberPublicClassMethds(Object o) {
+        return (int) Arrays.stream(o.getClass().getDeclaredMethods())
+                .filter(method -> Modifier.isPublic(method.getModifiers()))
                 .count();
     }
 }
