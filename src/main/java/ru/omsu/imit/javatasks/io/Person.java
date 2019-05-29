daedfb0a5660a51b4a7298464914372518007895
Roman <romanbelinsky99@gmail.com>
2019-05-29 07:58:48 +0600

    Add class Person.java

---
+++ src/main/java/ru/omsu/imit/javatasks/io/Person.java
@@ -0,0 +1,107 @@
+package ru.omsu.imit.javatasks.io;
+
+import java.util.Objects;
+
+public class Person {
+    private String surname;
+    private String name;
+    private String patronymic;
+    private int day;
+    private int month;
+    private int year;
+
+    public Person() {
+        this("None", "None", "None", 0, 0, 0);
+    }
+
+    public Person(String surname, String name, String patronymic, int day, int month, int year) {
+        setSurname(surname);
+        setName(name);
+        setPatronymic(patronymic);
+        setDay(day);
+        setMonth(month);
+        setYear(year);
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getFullDate() {
+        return day + "." + month + "." + year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return day == person.day &&
+                month == person.month &&
+                year == person.year &&
+                Objects.equals(surname, person.surname) &&
+                Objects.equals(name, person.name) &&
+                Objects.equals(patronymic, person.patronymic);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(surname, name, patronymic, day, month, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", day=" + day +
+                ", month=" + month +
+                ", year=" + year +
+                '}';
+    }
+}
