81638f3204f51b23a2dcba6b8e9e3a976c9fd31c
Roman <romanbelinsky99@gmail.com>
2019-05-29 08:07:12 +0600

    Add class House.java

---
+++ src/main/java/ru/omsu/imit/javatasks/io/House.java
@@ -0,0 +1,69 @@
+package ru.omsu.imit.javatasks.io;
+
+import java.util.List;
+import java.util.Objects;
+
+public class House {
+    private String cadastralHouseNumber;
+    private String address;
+    private Person seniorHousework;
+    private List<Flat> flats;
+
+    public void setCadastralHouseNumber(String cadastralHouseNumber) {
+        this.cadastralHouseNumber = cadastralHouseNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setSeniorHousework(Person seniorHousework) {
+        this.seniorHousework = seniorHousework;
+    }
+
+    public void setFlats(List<Flat> flats) {
+        this.flats = flats;
+    }
+
+    public String getCadastralHouseNumber() {
+        return cadastralHouseNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Person getSeniorHousework() {
+        return seniorHousework;
+    }
+
+    public List<Flat> getFlats() {
+        return flats;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        House house = (House) o;
+        return Objects.equals(cadastralHouseNumber, house.cadastralHouseNumber) &&
+                Objects.equals(address, house.address) &&
+                Objects.equals(seniorHousework, house.seniorHousework) &&
+                Objects.equals(flats, house.flats);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cadastralHouseNumber, address, seniorHousework, flats);
+    }
+
+    @Override
+    public String toString() {
+        return "House{" +
+                "cadastralHouseNumber='" + cadastralHouseNumber + '\'' +
+                ", address='" + address + '\'' +
+                ", seniorHousework=" + seniorHousework +
+                ", flats=" + flats +
+                '}';
+    }
+}
