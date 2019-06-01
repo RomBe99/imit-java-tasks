56b748e473d308f9fc59b04b5a40e0ecf7e4f88a
Roman <romanbelinsky99@gmail.com>
2019-06-01 11:00:03 +0600

    Fix and refactor lambdas-expresion in class StreamAPIDemo.java

--- src/main/java/ru/omsu/imit/javatasks/lambdas/StreamAPIDemo.java
+++ src/main/java/ru/omsu/imit/javatasks/lambdas/StreamAPIDemo.java
@@ -6,25 +6,51 @@ import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
 public class StreamAPIDemo extends LambdaDemo {
-    public final static UnaryOperator<List<?>>  deleteNullsFromObjectList = l -> l.stream().
-            filter(Objects::nonNull).collect(Collectors.toList());
-    public final static Function<Set<Integer>, Long> countPositiveNumbers = s -> s.stream().
-            filter(i -> i >= 0).count();
-    public final static Function<List<?>, Object[]> threeLastObjects = l -> l.stream().
-            skip((long) l.size() - 3).toArray();
-    public final static Function<List<Integer>, Integer> firstEvenNumber = l -> l.stream().
-            filter(i -> i % 2 == 0).findFirst().orElse(null);
-    public final static Function<Integer[], List<Integer>> squareWithoutRepeat = ints -> Arrays.stream(ints).
-        distinct().map(i -> i * i).collect(Collectors.toList());
-    public final static UnaryOperator<List<String>> allNotEmptyStrings = l -> l.stream().
-            filter(s -> s != null && !"".equals(s)).collect(Collectors.toList());
-    public final static Function<Set<String>, List<String>> sortSetDescending = s -> s.stream().
-            sorted(Comparator.comparingInt(String::length)).collect(Collectors.toList());
-    public final static Function<Set<Integer>, Integer> sumOfSquares = s -> s.stream().
-            mapToInt(i -> i * i).sum();
-    public final static Function<Collection<Human>, Integer> maxAge = c -> c.stream().
-            max(Comparator.comparingInt(Human::getAge)).orElseThrow().getAge();
-    public final static UnaryOperator<Collection<Human>> sortHumanCollection = c -> c.stream().
-            sorted((h1, h2) -> h1.getGender() == h2.getGender() ? 0 : h1.getGender() == Gender.MALE && h2.getGender() == Gender.FEMALE ? 1 : -1).
-            sorted(Comparator.comparingInt(Human::getAge)).collect(Collectors.toList());
+    public final static UnaryOperator<List<?>> deleteNullsFromObjectList = l -> l.stream()
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+
+    public final static Function<Set<Integer>, Long> countPositiveNumbers = s -> s.stream()
+            .filter(i -> i > 0)
+            .count();
+
+    public final static Function<List<?>, Object[]> threeLastObjects = l -> l.stream()
+            .skip((long) l.size() - 3)
+            .toArray();
+
+    public final static Function<List<Integer>, Integer> firstEvenNumber = l -> l.stream()
+            .filter(i -> i % 2 == 0)
+            .findFirst()
+            .orElse(null);
+
+    public final static Function<Integer[], List<Integer>> squareWithoutRepeat = ints -> Arrays
+            .stream(ints)
+            .map(i -> i * i)
+            .distinct()
+            .collect(Collectors.toList());
+
+    public final static UnaryOperator<List<String>> allNotEmptyStrings = l -> l.stream()
+            .filter(s -> s != null && !"".equals(s))
+            .collect(Collectors.toList());
+
+    public final static Function<Set<String>, List<String>> sortSetDescending = s -> s.stream()
+            .sorted((s1, s2) -> -s1.compareTo(s2))
+            .collect(Collectors.toList());
+
+    public final static Function<Set<Integer>, Integer> sumOfSquares = s -> s.stream()
+            .mapToInt(i -> i * i)
+            .sum();
+
+    public final static Function<Collection<Human>, Integer> maxAge = c -> c.stream()
+            .max(Comparator.comparingInt(Human::getAge))
+            .orElseThrow()
+            .getAge();
+
+    public final static UnaryOperator<Collection<Human>> sortHumanCollection = c -> c.stream()
+            .sorted(
+                    Comparator
+                            .comparing(Human::getGender)
+                            .thenComparing(Human::getAge)
+            )
+            .collect(Collectors.toList());
 }
